3c10a808f4627ee36ee7343157c0e90996475ed8
Rirn Kimhong <rirnkimhong@Rirns-MacBook-Air.local>
2025-05-12 01:31:43 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:recipe_app/app.dart';
+import 'package:recipe_app/modules/Home/controller/home_controller.dart';
 
 void main() {
+  Get.put(HomeController());
   runApp(const MyApp());
 }
